f9904189038e3aa1494828c6a1d33188d26d4cd2
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-09-15 10:37:19 +0300

    frist code

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function updateSquare() {
     const width = document.getElementById('width').value;
     const height = document.getElementById('height').value;
     if (width > 0 && height > 0) {
-        square.style.width = width + '%';
-        square.style.height = height + '%';
+        square.style.width = width + 'px';
+        square.style.height = height + 'px';
         square.style.backgroundColor = document.getElementById('color').value;
         square.style.borderRadius = document.getElementById('borderRadius').value + 'px';
     } 
@@ -12,12 +12,13 @@ function updateSquare() {
 
 function resetSquare() {
     const square = document.getElementById('square');
-    square.style.width = '30vh';
-    square.style.height = '40vh';
+    square.style.width = "30vw";
+    square.style.height = "40vh"; 
     square.style.backgroundColor = '#3498db';
-    square.style.borderRadius = '2px';
-    document.getElementById('width').value = '30';
-    document.getElementById('height').value = '40';
+    square.style.borderRadius = '10px';
+    square.style.border = '2px solid #2980b9';
+    document.getElementById('width').value = '200';
+    document.getElementById('height').value = '200';
     document.getElementById('color').value = '#3498db';
-    document.getElementById('borderRadius').value = '0';
+    document.getElementById('borderRadius').value = '10';
 }
